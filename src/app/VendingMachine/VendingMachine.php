e6cfbee1301f1631d2b65907cd236bde07f1fbec
Jordi Artau <jartau@interiorvista.net>
2021-08-04 11:39:42 +0200

    IMP - add missing method comments

--- src/app/VendingMachine/VendingMachine.php
+++ src/app/VendingMachine/VendingMachine.php
@@ -23,6 +23,7 @@ class VendingMachine
     }
 
     /**
+     * Add coin to counter stack
      * @param $value
      * @throws CoinException
      */
@@ -31,32 +32,56 @@ class VendingMachine
         $this->counter->insertCoin($value);
     }
 
+    /**
+     * Return the selected product code on dispenser
+     * @return string
+     * @throws ProductException
+     */
     public function getProductCode(): string
     {
         return $this->dispenser->getProductCode();
     }
 
+    /**
+     * Return the coin values to serve the change
+     * @return array
+     */
     public function getChange(): array
     {
         return $this->counter->getChange();
     }
 
+    /**
+     * Return the inserted coins values and empty the inserted stack
+     * @return array
+     */
     public function returnCoins(): array
     {
         return $this->counter->returnCoins();
     }
 
+    /**
+     * Return all stored coins on DB
+     * @return Collection
+     */
     public function getCoinsStatus(): Collection
     {
         return $this->counter->getCoinsStatus();
     }
 
+    /**
+     * Return all stored products on DB
+     * @return Collection
+     */
     public function getProductsStatus(): Collection
     {
         return $this->dispenser->getProductsStatus();
     }
 
     /**
+     * Select the product by code and calculate the necessary change to return
+     * trow a Coin exception if it can serve the necessary change
+     * trow a Product exceptions if product code is invalid or it have not enough stock
      * @param $code
      * @throws CoinException
      * @throws ProductException
@@ -73,22 +98,45 @@ class VendingMachine
 
     }
 
+    /**
+     * Update product stock and coin stock and earned value depends on selected product and calculated change
+     * @throws ProductException
+     */
     public function updateStates(): void
     {
         $this->counter->updateCoinStatus();
         $this->dispenser->updateProductStock();
     }
 
+    /**
+     * Add the quantity to value coin
+     * trow a Coin Exception if quantity or coin value are invalid
+     * @param int $value
+     * @param int $quantity
+     * @return bool
+     * @throws CoinException
+     */
     public function addCoins(int $value, int $quantity): bool
     {
         return $this->counter->addStock($value, $quantity);
     }
 
+    /**
+     * Add the quantity to product stock
+     * trow a Product Exception if quantity or product code are invalid
+     * @param string $code
+     * @param int $quantity
+     * @return bool
+     * @throws ProductException
+     */
     public function addProducts(string $code, int $quantity): bool
     {
         return $this->dispenser->addStock($code, $quantity);
     }
 
+    /**
+     * Set zero to all coins earned value
+     */
     public function collectCoins(): void
     {
         $this->counter->collectCoins();
